34f4855a1f87d7e54d37875b811fff31fd3a78b6
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-10-04 23:20:00 +0000

    Adding Tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,5 +2,17 @@
     // sidenav initialization
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format:"dd mmmm, yyyy",
+        i18n:{done: "select"}
+    });
+
+    //select inialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
   });
   
